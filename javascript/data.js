03886fa5e88ae4706fd207664eb1e2443c6c2d6f
ekenimoh elyan <ekenimohelyanpheta@gmail.com>
2021-07-08 21:04:24 +0100

    Add data array of objects

---
+++ javascript/data.js
@@ -0,0 +1,52 @@
+const Data = {
+	speakers: [
+{
+	key: 0,
+	speakerName: 'Julie Sweet',
+	speakerPosition: 'Chief Executive Officer of Accenture',
+	speakerImage: './Assets/Images/Julia-Sweet.jpeg',
+	speakerBio: "American business executive. She is chief executive officer of Accenture, a multinational professional services company. She is also a member of the Global Management Committee for Accenture. According to The New York Times, she is 'one of the most powerful women in corporate America.'",
+},
+
+{
+	key: 1,
+	speakerName: 'David Essel',
+	speakerPosition: 'Master Business, Relationship, and Success Coach, Counselor',
+	speakerImage: './Assets/Images/david-essel.jpeg',
+	speakerBio: "David is number one best-selling author (10), counselor, master life coach,  international speaker and minister whose mission is to positively affect 2 million people or more every day, in every area of life, regardless of their current circumstances.",
+},
+
+{
+	key: 2,
+	speakerName: 'David Meinz',
+	speakerPosition: 'America Personal Health Improvement Expert',
+	speakerImage: './Assets/Images/David-Meinz.jpeg',
+	speakerBio: "David Meinz specializes in consultations and workshops that enable people to improve their health. He presents information from his book Survival of the Fittest internationally to businesses and associations.",
+},
+
+	{
+		key: 3,
+		speakerName: 'Zonya Foco, RDN',
+		speakerPosition: 'America Nutrition Leader',
+		speakerImage: './Assets/Images/Zonya.jpeg',
+		speakerBio: "Zonya Foco, is a master of inspiration, motivation and visual humor, providing high energy presentations crossed with low maintenance solutions that help everyday people improve health, create balance and maximize energy.",
+	},
+	
+	{
+		key: 4,
+		speakerName: 'Robyn Benincasa',
+		speakerPosition: 'World Champion Adventure Racer, San Diego City Firefighter & CNN Hero',
+		speakerImage: './Assets/Images/robyn-Benincasa.jpeg',
+		speakerBio: "Robyn Benincasa has made an art form of extreme performance by competing and winning at the highest levels of sport and business. She brings her unique and inspiring perspective on teamwork to the corporate world while sharing her amazing stories as a world champion adventurer.",
+	},
+	
+	{
+		key: 5,
+		speakerName: 'Dr. Janet Brill',
+		speakerPosition: 'recognized expert in the field of health, wellness and cardiovascular disease prevention',
+		speakerImage: './Assets/Images/Dr-brill.jpeg',
+		speakerBio: "nationally recognized expert in the field of health, wellness and cardiovascular disease prevention and is frequently sought after by the media as a trusted source of nutrition and fitness information. Her straightforward heart health advice that has helped thousands of Americans eat better and lead healthier, more fulfilling lives.",
+	},
+	],
+};
+export default Data;
